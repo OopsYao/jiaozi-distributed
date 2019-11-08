d51605f0e4beefa90007f7f37e72de19178b4b61
xyao <buzz@smail.swufe.edu.cn>
2019-11-08 14:48:49 +0800

    comment

--- start.ps1
+++ start.ps1
@@ -1,13 +1,17 @@
 $WORKING_DIR = $PWD
+# Server程序目录
 $SERVER_HOME = "${WORKING_DIR}/server/cmake-build-debug"
 
+# 创建logs目录
 New-Item -ItemType Directory -Force -Path logs;
 
+# 启动Server Job
 $server = Start-Job -Name 'Server' -ScriptBlock {
     Set-Location $using:SERVER_HOME
     & ./server.exe
 }
 
+# 根据Server配置中的节点数目启动相应数目的Job
 $nodeNum = (Get-Content "${SERVER_HOME}/server.json" | ConvertFrom-Json ).mainConfig.nodeCount
 $array = 1..$nodeNum
 foreach ($index in $array) {
@@ -17,6 +21,8 @@ foreach ($index in $array) {
     }
 }
 
+# 在Server Job结束后删除节点的Job
+# TODO 但好像不起作用?
 $completed = Register-ObjectEvent -InputObject $server -EventName StateChanged -Action {
     Write-Host ('Job #{0} ({1}) complete.' -f $sender.Id, $sender.Name)
     Get-Job -Name Node*, Server | Remove-Job -Force
