3e128270a0c1c7b505808f07c1c8ea84182df907
xyao <buzz@smail.swufe.edu.cn>
2019-11-09 22:22:12 +0800

    score.py

---
+++ score.py
@@ -0,0 +1,53 @@
+import json
+
+"""计算单组测试得分（总分为各组测试得分和）"""
+
+
+server_home = "./server/"
+cmake_build_debug_dir = server_home + "cmake-build-debug/"
+
+
+def get_sigma():
+    """从测试服务器配置server.json中读取允许最大延时sigma"""
+    server = json.load(open(cmake_build_debug_dir + "server.json"))
+    return server["mainConfig"]["timeOut"]
+
+
+def get_test_msg_num():
+    """获取要求传达的msg的数量"""
+    test = json.load(open(server_home + "test.json"))
+    # TODO 原score.py中将数量除以了2，没弄明白
+    return len(test)
+
+
+def get_delay_list():
+    """实际到达的msg的延迟（包含了可能超时到达的）"""
+    delay_list = []
+    with open(cmake_build_debug_dir + "attach.log") as log:
+        for line in log.readlines():
+            # hash(or data), 收到时间, 延时
+            _, _, d = line.split(",")
+            delay_list.append(float(d))
+    return delay_list
+
+
+def main():
+    test_msg_num = get_test_msg_num()
+    sigma = get_sigma()
+    delay_list = get_delay_list()
+
+    tao = [delay for delay in delay_list if delay <= sigma]
+    tao_avg = sum(tao) / len(tao) if len(tao) != 0 else 0
+    D_tao = sum([(i - tao_avg) ** 2 for i in tao]) / len(tao) if len(tao) != 0 else 0
+
+    eta = len(tao) / test_msg_num
+
+    print(f"With sigma = {sigma}")
+    print({"eta": f"{eta:.001%}", "Mean of tao": tao_avg, "Variance of tao": D_tao})
+
+    G = ((sigma * 2 - tao_avg) / sigma * 3.5 + 0.5 / (D_tao + 1)) * eta ** 2.5 * 10
+    print("Final score: G =", G)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tool/caller.py
@@ -0,0 +1,13 @@
+def build_channel(target_node, channel_type):
+    """建立与target_node之间的通道"""
+    pass
+
+
+def destroy_channel(channel_id):
+    """摧毁通道"""
+    pass
+
+
+def send_message(message, channel_id):
+    """通过某一条channel传输信息"""
+    pass
